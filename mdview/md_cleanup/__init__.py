48e61f82023fabe855d599a2be73b4ad1038c20a
Sebastian Humenda <shumenda@gmx.de>
2024-01-21 18:05:04 +0100

    [md_cleanup] strip nonbreaking spaces
    md_cleanup now strips non-breaking spaces and replaces them by normal
    spaces.

--- mdview/md_cleanup/__init__.py
+++ mdview/md_cleanup/__init__.py
@@ -22,8 +22,14 @@ def strip_divs(contents):
                      if not line.lstrip().startswith(":::")
     )
 
-def strip_unwantend_formatting(contents):
-    """Strip unwatned formatting from inline elements."""
+def strip_nonbreaking_space(text):
+    return text.replace("\xa0", " ")
+
+def strip_unwanted_css_from_elements(contents):
+    """In Pandoc's Markdown, additional attributes, including CSS-classes for
+    visual formatting, can be attached. They are not useful if the document is
+    not meant to be exported with a specific style sheet and are hence
+    stripped."""
     for pattern in REPLACE_PATTERNS:
         contents = pattern.sub(r"\1", contents)
     return contents
@@ -32,22 +38,28 @@ def strip_unwantend_formatting(contents):
 def md_cleanup(document):
     """Take a document a s a string and perform all formatting changes defined
     in this module."""
-    document = strip_unwantend_formatting(document)
+    document = strip_nonbreaking_space(document)
+    document = strip_unwanted_css_from_elements(document)
     document = strip_divs(document)
     return document
 
 def main():
     if len(sys.argv) != 2:
-        print("E: requires one argument, file name")
+        print("E: requires one argument, file name or `-` for stdin")
         sys.exit(1)
     text = None
-    if sys.argv[1] == "-":
+    input_file = sys.argv[1]
+    if input_file == "-":
         text = sys.stdin.read()
     else:
-        with open(sys.argv[1], encoding="UTF-8") as fhandle:
+        with open(input_file, encoding="UTF-8") as fhandle:
             text = fhandle.read()
     text = md_cleanup(text)
-    print(text)
+    if input_file == "-":
+        print(text)
+    else:
+        with open(input_file, "w", encoding="UTF-8") as fhandle:
+            fhandle.write(text)
 
 if __name__ == '__main__':
     main()
